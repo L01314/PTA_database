db2e1765604ca2f8f0c8a7e652063b525cbd83e2
优秀aing <79087104+L01314@users.noreply.github.com>
2022-05-26 22:05:53 +0800

    Update 7-1 输出最高分学生信息.cpp

--- 7-1 输出最高分学生信息.cpp
+++ 7-1 输出最高分学生信息.cpp
@@ -1,21 +1,29 @@
 #include<stdio.h>
 int n,i,j,k;
 struct node
-{
+{   
 	char x[6];
-	char name[11];
-	int c;
-}b[100],max;
+	char name[22];
+	int c,a,b,d;
+	float 
+}b[100],temp;
 int main()
 {
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
-		scanf("%s %s %d",b[i].x,b[i].name,&b[i].c);
-	max=b[0];
-	for(i=0;i<n;i++)
 	{
-		if(b[i].c>max.c)
-			max=b[i];
+		scanf("%s %s %d",b[i].x,b[i].name,&b[i].c);
 	}
-	printf("%s %s %d",b[i].x,b[i].name,b[i].c);
+	
+	
+	for(i=0;i<n;i++)
+	{ 
+		for(j=0;j<i;j++)
+		{
+			
+			if(b[j].c>b[j+1].c)
+				temp=b[j];b[j]=b[j+1];b[j+1]=temp;
+		}
+	}
+	printf("%s %s %d",max.x,max.name,max.c);
 }
